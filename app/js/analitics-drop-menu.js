78f66e5b63c06c74b5e9c3ea25b7796cf6576dc1
Alexander <alexandertsyrkin@gmail.com>
2023-10-24 16:19:11 +0400

    feat: fix analitics section

--- app/js/analitics-drop-menu.js
+++ app/js/analitics-drop-menu.js
@@ -1,3 +1,11 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
+   const analiticContent = document.querySelector(".analitics__content");
+   const analiticCard = analiticContent.querySelectorAll("li");
+   const analiticDropMenu = document.querySelector(".dropdown-container");
+   analiticCard.forEach((el) => {
+      el.addEventListener("click", () => {
+         analiticDropMenu.classList.add("activated");
+      });
+   });
 });
