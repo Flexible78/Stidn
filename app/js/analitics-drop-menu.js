ddf5598ffd96a7c4a2e5a4218908b8a07c8994bf
Alexander <alexandertsyrkin@gmail.com>
2023-10-24 21:31:24 +0400

    feat: fix analitics section

--- app/js/analitics-drop-menu.js
+++ app/js/analitics-drop-menu.js
@@ -3,9 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
    const analiticContent = document.querySelector(".analitics__content");
    const analiticCard = analiticContent.querySelectorAll("li");
    const analiticDropMenu = document.querySelector(".dropdown-container");
+   let timeoutId;
    analiticCard.forEach((el) => {
-      el.addEventListener("click", () => {
-         analiticDropMenu.classList.add("activated");
+      el.addEventListener("mouseover", () => {
+         analiticDropMenu.classList.toggle("activated");
       });
+      timeoutId = setTimeout(() => {
+         analiticDropMenu.classList.remove("activated");
+      }, 1000);
    });
+   analiticContent.forEach((el) => {
+      el.addEventListener("mouseover", () => {
+         analiticDropMenu.classList.remove("activated");
+      });
+      timeoutId = setTimeout(() => {
+         analiticDropMenu.classList.remove("activated");
+      }, 1000);
+   });
+   analiticDropMenu.addEventListener("mouseover", () => {
+      analiticDropMenu.classList.remove("activated");
+   });
+   timeoutId = setTimeout(() => {
+      analiticDropMenu.classList.remove("activated");
+   }, 1000);
 });
