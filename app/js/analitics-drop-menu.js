1440e6fb5b504fcf17c3e75163e8b6f1cdb77199
Alexander <alexandertsyrkin@gmail.com>
2023-10-29 11:17:45 +0400

    feat: analitics section fix

--- app/js/analitics-drop-menu.js
+++ app/js/analitics-drop-menu.js
@@ -1,16 +1,21 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-   const analiticContent = document.querySelector(".analitics__content");
-   const analiticCard = analiticContent.querySelectorAll("li");
-   const analiticDropMenu = document.querySelector(".dropdown-container");
+   const analiticContent = document.querySelector(".analitics__list");
+   const analiticCard = analiticContent.querySelectorAll(".analitics__card");
+   const analiticDropMenu = document.querySelector(".analitics__submenu");
    analiticCard.forEach((el) => {
       el.addEventListener("mouseenter", () => {
-         analiticDropMenu.classList.toggle("activated");
+         el.classList.add("active");
+      });
+   });
+   analiticCard.forEach((el) => {
+      el.addEventListener("mouseleave", () => {
+         analiticDropMenu.classList.remove("active");
       });
    });
    analiticContent.forEach((el) => {
       el.addEventListener("mouseenter", () => {
-         analiticDropMenu.classList.remove("activated");
+         analiticDropMenu.classList.remove("active");
       });
    });
 });
