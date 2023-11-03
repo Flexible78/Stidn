709b85c3cda6de489878b0ffa2c7f41df645557e
Alexander <alexandertsyrkin@gmail.com>
2023-11-04 01:38:08 +0400

    feat: fix analitics section

--- app/js/analitics-drop-menu.js
+++ app/js/analitics-drop-menu.js
@@ -1,21 +1,49 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
-   const analiticContent = document.querySelector(".analitics__list");
-   const analiticCard = analiticContent.querySelectorAll(".analitics__card");
-   const analiticDropMenu = document.querySelector(".analitics__submenu");
-   analiticCard.forEach((el) => {
-      el.addEventListener("mouseenter", () => {
-         el.classList.add("active");
-      });
+   const analitics = document.querySelector(".analitics");
+   analitics.querySelector("ul").classList.add("analitics__list");
+   const analiticsList = analitics.querySelector(".analitics__list");
+   analiticsList.querySelectorAll(".analitics__list > li").forEach(cardItem => {
+      cardItem.classList.add("analitics__card");
+      cardItem.firstElementChild.classList.add("analitics__main-menu");
+
+      if (cardItem.children.length > 1) {
+         cardItem.lastElementChild.classList.add("analitics__submenu");
+      }
+      const card = cardItem.querySelector(".analitics__main-menu");
+      let analiticsLogo;
+      if (card.querySelector("img")) {
+         analiticsLogo = card.querySelector("img");
+      } else if (card.querySelector("svg")) {
+         analiticsLogo = card.querySelector("svg");
+      }
+      const logoWrapper = card.querySelector("div");
+      logoWrapper.classList.add("analitics__logo");
+      /* analiticsLogo.before(logoWrapper);
+       logoWrapper.appendChild(analiticsLogo); */
+
+      let analiticsText;
+      let stringOfTheNextElementAfterLogo = logoWrapper.nextElementSibling.tagName.toLowerCase();
+
+      if (stringOfTheNextElementAfterLogo === "p" || stringOfTheNextElementAfterLogo === "div") {
+         analiticsText = logoWrapper.nextElementSibling;
+         analiticsText.classList.add("analitics__text");
+      }
    });
-   analiticCard.forEach((el) => {
-      el.addEventListener("mouseleave", () => {
-         analiticDropMenu.classList.remove("active");
+   /* Submenu */
+   if (analiticsList) {
+      const analiticsCards = analiticsList.querySelectorAll(".analitics__card");
+
+      analiticsCards.forEach(card => {
+         if (card.children.length > 1) {
+            const submenu = card.querySelector(".analitics__submenu");
+            card.addEventListener("mouseenter", () => {
+               card.classList.add("active");
+            });
+            card.addEventListener("mouseleave", () => {
+               card.classList.remove("active");
+            });
+         }
       });
-   });
-   analiticContent.forEach((el) => {
-      el.addEventListener("mouseenter", () => {
-         analiticDropMenu.classList.remove("active");
-      });
-   });
+   }
 });
